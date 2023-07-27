1499bef4705224428b5703333111046fe785d66c
fpetri115 <francescopetri115@gmail.com>
2023-07-27 09:15:34 +0100

    igm stuff

--- lbg_forecast/igm.py
+++ lbg_forecast/igm.py
@@ -1,19 +1,20 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 #(Wavelengths shortward of Lyman Limit)
 def lyman_continuum_tau(l_obs, z_em):
 
     l_lylim = 912
-    x_c = l_obs/l_lylim
 
+    x_c = l_obs/l_lylim
     x_em = 1+z_em
 
-    tau_eff_exp = np.exp(-1*(0.25*(x_c**3)*(x_em**0.46 - x_c**0.46) \
+    tau_eff = (0.25*(x_c**3)*(x_em**0.46 - x_c**0.46) \
                 +9.4*(x_c**1.5)*(x_em**0.18 - x_c**0.18) \
                 -0.7*(x_c**3)*(x_c**(-1.32) - x_em**(-1.32)) \
-                -0.023*(x_em**1.68 - x_c**1.68)))
+                -0.023*(x_em**1.68 - x_c**1.68))
     
-    tau_eff_exp = np.where(l_obs/(1+z_em) < l_lylim, tau_eff_exp*1, tau_eff_exp*0)
+    tau_eff_exp = np.where(l_obs < l_lylim*(1+z_em), np.exp(-1*tau_eff), 0)
     
     return tau_eff_exp
 
@@ -42,9 +43,13 @@ def lyman_series_line_tau(l_obs, z_em):
     l_rest = np.tile(l_rest, (no_lines, 1)) #tiles into rows
     l_obs = np.tile(l_obs, (no_lines, 1))
 
-    bool = np.logical_and(l_rest < ly_series, l_rest > ly_seriesp1)
+    bool = np.logical_and(l_rest > ly_series, l_rest < ly_seriesp1)
 
     tau_eff_exp_all = np.exp(-1*(coeffs*((l_obs/ly_series)**(3.46))))
     tau_eff_exp_reduced = tau_eff_exp_all*bool #true, false turns to 1, 0
 
     return np.sum(tau_eff_exp_reduced, axis=0) #sum columns
+
+def lyalpha_transmission(z):
+
+    return np.exp(-3.6e-3*(1+z)**3.46)
