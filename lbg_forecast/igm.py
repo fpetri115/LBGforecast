3652645f760ed89c267c17ba6eea3d431fb3d515
fpetri115 <francescopetri115@gmail.com>
2023-07-27 12:18:01 +0100

    plotting transmission curves

--- lbg_forecast/igm.py
+++ lbg_forecast/igm.py
@@ -14,22 +14,18 @@ def lyman_continuum_tau(l_obs, z_em):
                 -0.7*(x_c**3)*(x_c**(-1.32) - x_em**(-1.32)) \
                 -0.023*(x_em**1.68 - x_c**1.68))
     
-    tau_eff_exp = np.where(l_obs < l_lylim*(1+z_em), np.exp(-1*tau_eff), 0)
+    tau_eff = np.where(l_obs < l_lylim*(1+z_em), tau_eff, 0)
     
-    return tau_eff_exp
+    return tau_eff
 
 def lyman_series_line_tau(l_obs, z_em):
 
-    l_lylim = 912
     ly_series = np.array([1215.67, 1025.72, 972.537, 949.743, 937.803,
           930.748, 926.226, 923.150, 920.963, 919.352,
           918.129, 917.181, 916.429, 915.824, 915.329,
           914.919, 914.576])
     
     ly_series = np.reshape(ly_series, (17, 1))
-    ly_seriesp1 = ly_series[1:]
-    ly_seriesp1 = np.append(ly_seriesp1, l_lylim)
-    ly_seriesp1 = np.reshape(ly_seriesp1, (17, 1))
     
     coeffs = np.array([0.0036,0.0017,0.0011846,0.0009410,0.0007960,
        0.0006967,0.0006236,0.0005665,0.0005200,0.0004817,
@@ -43,13 +39,35 @@ def lyman_series_line_tau(l_obs, z_em):
     l_rest = np.tile(l_rest, (no_lines, 1)) #tiles into rows
     l_obs = np.tile(l_obs, (no_lines, 1))
 
-    bool = np.logical_and(l_rest > ly_series, l_rest < ly_seriesp1)
+    bool = (l_rest < ly_series)
 
-    tau_eff_exp_all = np.exp(-1*(coeffs*((l_obs/ly_series)**(3.46))))
-    tau_eff_exp_reduced = tau_eff_exp_all*bool #true, false turns to 1, 0
+    tau_eff_all = coeffs*((l_obs/ly_series)**(3.46))
+    tau_eff_reduced = tau_eff_all*bool #true, false turns to 1, 0
 
-    return np.sum(tau_eff_exp_reduced, axis=0) #sum columns
+    return np.sum(tau_eff_reduced, axis=0) #sum columns (optical depths are additive)
 
-def lyalpha_transmission(z):
+def lyalpha_transmission(z, tau=False):
 
-    return np.exp(-3.6e-3*(1+z)**3.46)
+    if(tau):
+        return -np.log(np.exp(-3.6e-3*(1+z)**3.46))
+    else:
+        return np.exp(-3.6e-3*(1+z)**3.46)
+    
+def plot_lya_transmission(zmin, zmax, tau=False):
+
+    z_space = np.linspace(zmin, zmax, 1000)
+    return plt.plot(z_space, lyalpha_transmission(z_space, tau))
+
+def plot_transmission_curves(z):
+
+    l_min = 750*(1+z)
+    l_max = 1216*(1+z)
+    l_obs = np.linspace(l_min, l_max, int(l_max-l_min)*10)
+    tau_eff = lyman_continuum_tau(l_obs, z)
+    tau_eff_series = lyman_series_line_tau(l_obs, z)
+
+    plt.figure(figsize=(10,5))
+    plt.plot(l_obs, np.exp(-1*(tau_eff + tau_eff_series)))
+    plt.plot(l_obs, np.exp(-1*(tau_eff + tau_eff_series))*1.5)
+    plt.plot(l_obs, np.exp(-1*(tau_eff + tau_eff_series))*0.5)
+
