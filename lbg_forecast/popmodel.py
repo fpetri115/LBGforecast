1d21b28abc482367a0c5f639481bbdf3552673bd
fpetri115 <francescopetri115@gmail.com>
2024-01-08 13:41:35 +0000

    generated new nzs with new priors

--- lbg_forecast/popmodel.py
+++ lbg_forecast/popmodel.py
@@ -3,6 +3,7 @@ import math
 import matplotlib.pyplot as plt
 from astropy.cosmology import WMAP9 as cosmo
 import lbg_forecast.distributions as dstr
+import lbg_forecast.priors as pr
 
 #draws single galaxy sample given a set of hyper parmeters (hparams)
 #hparams are found using sample_hyper_parameters() in hyperparams.py
@@ -30,20 +31,28 @@ def galaxy_population_model(hparams):
     return realisation
 
 # (VECTORISED)
-def galaxy_population_model_vec(hparams, nsamples):
+def galaxy_population_model_vec(hparams, prior_params, nsamples):
+
+    z_grid, logm_grid, priors, grid_params = prior_params
+    z_samples, m_samples = pr.sample_priors(z_grid, logm_grid, priors, grid_params, nsamples, plotting=False)
     
     i = 0
     realisation_list = []
     for hparam in hparams:
 
-        if(i==0): 
-            realisation_list.append(np.vstack(dstr.sample_prior_vec(hparam, nsamples, vectorise_bounds=0)))
+        if(i == 0):
+            realisation_list.append(np.vstack(z_samples))
             tuniv = []
-            for z in realisation_list[0]:
+            for z in z_samples:
                 tuniv.append(cosmo.age(z).value)
-            tuniv = np.reshape(np.log10(np.asarray(tuniv)), (nsamples,))   
-        elif(i==1):
+            tuniv = np.reshape(np.log10(np.asarray(tuniv)), (nsamples,)) 
+                
+        elif(i == 1):
             realisation_list.append(np.transpose(dstr.sample_prior_vec(hparam, nsamples, vectorise_bounds=tuniv)))
+
+        elif(i == 14):
+            realisation_list.append(np.vstack(m_samples))
+        
         else:
             realisation_list.append(np.vstack(dstr.sample_prior_vec(hparam, nsamples, vectorise_bounds=0)))
 
