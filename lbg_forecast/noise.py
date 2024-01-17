eacf25e5ce425e395eb093457bb9c36dd2f4fd5e
fpetri115 <francescopetri115@gmail.com>
2024-01-17 12:36:01 +0000

    better detection

--- lbg_forecast/noise.py
+++ lbg_forecast/noise.py
@@ -5,20 +5,22 @@ import numpy as np
 def select_u_dropouts(observed_catalog):
     
     udrop = observed_catalog.copy(deep=True)
+
     udrop = udrop.dropna(axis=0, subset=['i5'])
-    #udrop = udrop.dropna(axis=0, subset=['r5'])
-    #udrop = udrop.dropna(axis=0, subset=['g5'])
-    #udrop = udrop.drop(udrop[np.isnan(udrop.u2) == False].index)
+    udrop = udrop.dropna(axis=0, subset=['r5'])
+    udrop = udrop.dropna(axis=0, subset=['g5'])
+    udrop = udrop.dropna(axis=0, subset=['u5'])
 
     return udrop.filter(['u','g','r','i','z','y'])
 
 def select_g_dropouts(observed_catalog):
     
     gdrop = observed_catalog.copy(deep=True)
+
     gdrop = gdrop.dropna(axis=0, subset=['i5'])
-    #gdrop = gdrop.dropna(axis=0, subset=['r5'])
-    #gdrop = gdrop.drop(gdrop[np.isnan(gdrop.g2) == False].index)
-    gdrop = gdrop.drop(gdrop[np.isnan(gdrop.u2) == False].index)
+    gdrop = gdrop.dropna(axis=0, subset=['r5'])
+    gdrop = gdrop.dropna(axis=0, subset=['g5'])
+    #gdrop = gdrop.drop(gdrop[np.isnan(gdrop.u2) == False].index)
 
     
     return gdrop.filter(['u','g','r','i','z','y'])
@@ -26,10 +28,10 @@ def select_g_dropouts(observed_catalog):
 def select_r_dropouts(observed_catalog):
 
     rdrop = observed_catalog.copy(deep=True)
-    #rdrop = rdrop.dropna(axis=0, subset=['z5'])
+    rdrop = rdrop.dropna(axis=0, subset=['z5'])
     rdrop = rdrop.dropna(axis=0, subset=['i5'])
-    #rdrop = rdrop.drop(rdrop[np.isnan(rdrop.r2) == False].index)
-    rdrop = rdrop.drop(rdrop[np.isnan(rdrop.g2) == False].index)
+    rdrop = rdrop.dropna(axis=0, subset=['r5'])
+    #rdrop = rdrop.drop(rdrop[np.isnan(rdrop.g2) == False].index)
     
     return rdrop.filter(['u','g','r','i','z','y'])
 
@@ -38,12 +40,6 @@ def get_noisy_magnitudes(sps_params, noiseless_photometry, random_state=42):
     #noiseless_photometry = np.load("/Users/fpetri/repos/LBGforecast/data/data/training_data.npy")[:1000000, :6]
     #sps_params = np.load("/Users/fpetri/repos/LBGforecast/data/data/training_params.npy")[:1000000, :]
     catalog = pd.DataFrame(noiseless_photometry, columns=['u', 'g', 'r', 'i', 'z', 'y'])
-
-    # remove photometry brighter than brightness cut
-    brightness_cut = 18
-    for column in ['u','g','r','i','z','y']:
-        catalog = catalog.drop(catalog[catalog[column] < brightness_cut].index)
-
 
     errModel = LsstErrorModel(sigLim=0, absFlux=True)
     sig5detections = LsstErrorModel(sigLim=5)
@@ -59,7 +55,9 @@ def get_noisy_magnitudes(sps_params, noiseless_photometry, random_state=42):
 
     observed_catalog = observed_catalog.join(catalog_sig5).join(catalog_sig2)
     
+    brightness_cut = 19
     observed_catalog.dropna(axis=0, subset=['i5'], inplace=True) #require 5sigma detection in i band
+    observed_catalog.drop(observed_catalog[observed_catalog['i5'] < brightness_cut].index, inplace=True)
 
     udrop = select_u_dropouts(observed_catalog)
     gdrop = select_g_dropouts(observed_catalog)
