ad426f14bdec8b6526e7ec8f7da73613ba1f4ff1
fpetri115 <francescopetri115@gmail.com>
2024-02-08 11:26:18 +0000

    hyper parameter plotting

---
+++ lbg_forecast/hyperparameters.py
@@ -0,0 +1,143 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+
+def sample_prior_parameters(nsamples, bounds, sigma_bounds):
+    """Sample prior parameters (hyperparameters) for
+    SPS model. 
+
+    :param nsamples:
+        Number of samples
+
+    :param bounds:
+        (no. priors, 2) shape array with minimum and maximum
+        mean for gaussian distribution for a given prior in 
+        each row. The last rows are for uniform priors, where
+        each row is then the minimum and maximum of that uniform
+        distribution. The number of gaussian priors is determined
+        by the length of the sigma_bounds parameter.
+
+    :param sigma_bounds:
+        (no. gaussian priors, 2) shape array with minimum and maximum
+        standard deviation for gaussian priors. len(sigma_bounds) gives
+        number of gaussian priors. So for any rows of parameter bounds
+        with index greater then len(sigma_bounds) will be treated as
+        uniform priors
+
+    :returns prior_parameters:
+        (nsamples, len(bounds)) shape array of hyperparameters. Each row is a
+        different reaslisation of the hyperparameters.
+        Number of rows = nsamples. Columns are parameters,
+        in order given by (mu1, sig1, mu2, sig2, ... ) up
+        to uniform prior parameters which then go as
+        (min1, max1, min2, max2 ... )
+    
+    """
+    n_gaussian_priors = len(sigma_bounds)
+    n_priors = len(bounds)
+    n_prior_parameters = int(2*n_priors)
+    prior_parameters = np.empty((nsamples, n_prior_parameters))
+
+    indx = 0
+    #loop over gaussian priors
+    while(indx < n_gaussian_priors):
+
+        parameter_bounds = bounds[indx, :]
+        parameter_sigma_bounds = sigma_bounds[indx, :]
+        prior_parameters[:, int(2*indx)] = np.random.uniform(parameter_bounds[0], parameter_bounds[1], (nsamples,))
+        prior_parameters[:, int(2*indx+1)] = np.random.uniform(parameter_sigma_bounds[0], parameter_sigma_bounds[1], (nsamples,)) 
+
+        indx+=1
+
+    #loop over uniform priors
+    while(indx < n_priors):
+
+        parameter_bounds = bounds[indx, :]
+        prior_parameters[:, int(2*indx)] = np.ones((nsamples,))*parameter_bounds[0]
+        prior_parameters[:, int(2*indx+1)] = np.ones((nsamples,))*parameter_bounds[1]
+
+        indx+=1
+
+    return prior_parameters
+
+def default_hyperparameter_bounds():
+    """Some default parameters for sample_prior_parameters()
+    """
+
+    hyperparameter_mu_bounds = np.array([[-2.5, 0.5],  #logzsol
+                                    [0.0, 2.0],      #dust1
+                                    [0.0, 4.0],      #dust2
+                                    [-1.0, 0.4],     #dust_index
+                                    [1.0, 1.0],      #igm_factor 
+                                    [-4.0, -1.0],    #gas_log_u
+                                    [-5.0, 1.0],     #log10(fagn)
+                                    [5, 150],        #agn_tau
+                                    [2, 10],         #nu
+                                    [0.1, 1.0]       #sig
+    ])
+
+    hyperparameter_sigma_max = np.array([[0.01, 3.0], #logzsol
+                                        [0.01, 2.0], #dust1
+                                        [0.01, 4.0], #dust2
+                                        [0.01, 0.5], #dust_index
+                                        [0.01, 2.0], #igm_factor 
+                                        [0.01, 3.0], #gas_log_u
+                                        [0.01, 6.0], #log10(fagn)
+                                        [0.1, 145]   #agn_tau
+    ])
+
+    return hyperparameter_mu_bounds, hyperparameter_sigma_max
+
+def plot_hyperparameters(prior_parameters, rows=5, nbins=20):
+    """Plots output of sample_prior_parameters()
+    
+    """
+    nparams = prior_parameters.shape[1]
+    columns = math.ceil(nparams/rows)
+    total_plots = nparams
+    grid = rows*columns
+
+    names = np.array(["logzsol_mu", "logzsol_sig", "dust1_mu", "dust1_sig", "dust2_mu", "dust2_sig", "dust_index_mu", "dust_index_sig",
+                      "igm_factor_mu", "igm_factor_sig", "gas_logu_mu", "gas_logu_sig", "logfagn_mu", "logfagn_sig", "agn_tau_mu", "agn_tau_sig", 
+                        "nu_min", "nu_max", "sig_min", "sig_max"])
+
+
+    fig1, axes1 = plt.subplots(rows, columns, figsize=(20,20), sharex=False, sharey=False)
+
+    i = 0
+    j = 0
+    plot_no = 0
+    name_count = 0
+    col = 0
+    while(col < nparams):
+
+        if(i > rows - 1):
+            j+=1
+            i=0
+
+        if(plot_no > total_plots):
+            axes1[i, j].set_axis_off()
+
+        else:
+            axes1[i, j].hist(prior_parameters[:,col], density = True, bins=nbins)
+            axes1[i, j].set_xlabel(names[name_count])
+            axes1[i, j].set_ylabel("$p(z)$")
+        
+        i+=1
+        plot_no += 1
+        name_count += 1
+        col += 1
+
+    #clear blank figures
+    no_empty_plots = grid - nparams
+    i = 0
+    while(i < no_empty_plots):
+        axes1[rows - i - 1, columns - 1].set_axis_off()
+        i+=1
+    
+    plt.tight_layout()
+
+
+
+
