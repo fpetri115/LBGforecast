d06cd68ab133a5e841a8d90de6dfdea001eedfbf
fpetri115 <francescopetri115@gmail.com>
2024-04-11 11:18:55 +0100

    changed file saving

--- generate_training_data.py
+++ generate_training_data.py
@@ -17,21 +17,13 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-run_count = 0
 ngals = int(sys.argv[1])
 path = sys.argv[2]
+run_count = int(sys.argv[3])
 
 
 if(rank == 0):
     print("nprocesses: ", size)
-
-#counts how many individual files of a given type there are (i.e. number of simulation runs)
-files = os.listdir(path+"simulation_data")
-for file in files:
-    if("sps_parameters" in file):
-        run_count+=1
-print("number of runs already in directory: ", run_count)
-
 
 hyperparameter_mu_bounds, hyperparameter_sigma_max = hyp.uniform_hyperparameter_bounds()
 prior_parameters = hyp.sample_prior_parameters(1, hyperparameter_mu_bounds, hyperparameter_sigma_max)
