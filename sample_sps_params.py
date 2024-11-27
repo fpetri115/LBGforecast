010f6324d58c18c03f3a07aa82a3a0967855e0ac
fpetri115 <francescopetri115@gmail.com>
2024-11-27 09:29:55 +0000

    modified mpi memory buffers

--- sample_sps_params.py
+++ sample_sps_params.py
@@ -7,18 +7,22 @@ import lbg_forecast.dust_priors as dpr
 import lbg_forecast.priors_gp_csfrd as gpsf
 from mpi4py import MPI
 
+NSPS_PARAMS = 17
+
+#initialise MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
-
 if(rank == 0):
     print("nprocesses: ", size)
 
+#collect inputs
 ngals = int(sys.argv[1])
 nrealisations = int(sys.argv[2])
 run = sys.argv[3]
 path = sys.argv[4]
 
+#load prior information
 if(rank == 0):
     print("Loading Priors ... ", flush=True)
     mass_function_prior = gpmf.MassFunctionPrior(path=path[:-1])
@@ -29,25 +33,33 @@ else:
     dust_prior = None
     csfrd_prior = None
 
+#broadcast prior information to processes
 mass_function_prior = comm.bcast(mass_function_prior, root=0)
 dust_prior = comm.bcast(dust_prior, root=0)
 csfrd_prior = comm.bcast(csfrd_prior, root=0)
 
+#setup memory
+sps_buf = np.zeros((nrealisations, ngals, NSPS_PARAMS))
+recv_buf = None
+if(rank == 0):
+    recv_buf = np.zeros((nrealisations * size, ngals, NSPS_PARAMS))
+
+#sample SPS parameters
 if(rank == 0):
     print("Begin Sampling ... ", flush=True)
 
-spsp = []
 for n in range(nrealisations):
     sps_params = pop.generate_sps_parameters(ngals, mass_function_prior, dust_prior, csfrd_prior)
-    spsp.append(sps_params)
+    sps_buf[n, :, :] = sps_params
     if(rank == 0):
         print("Realisation: ", n+1, flush=True)
 
 if(rank == 0):
     print("Waiting For Other Processes ... ", flush=True)
 
-all_realisations = comm.gather(spsp, root=0)
+#gather arrays
+comm.Gather(sps_buf, recv_buf, root=0)
 
 if(rank == 0):
-    np.save(path+"sps_parameter_samples/sps_"+run+".npy",np.concatenate(np.array(all_realisations)))
+    np.save(path+"sps_parameter_samples/sps_"+run+".npy", recv_buf)
     print("Complete.", flush=True)
