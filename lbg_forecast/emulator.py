8fd7c8f6929e01f1f60b78b5b4eeb43b754374c1
fpetri115 <francescopetri115@gmail.com>
2024-12-07 15:34:35 +0000

    MORE PRINT STATEMENTS

--- lbg_forecast/emulator.py
+++ lbg_forecast/emulator.py
@@ -91,18 +91,21 @@ class fsps_emulator:
 
         outputs = []
         layers = [tf.divide(tf.subtract(parameters, model.parameters_shift), model.parameters_scale)]
+        #no diff
         for i in range(model.n_layers - 1):
             
             # linear network operation
             outputs.append(tf.add(tf.matmul(layers[-1], model.W[i]), model.b[i]))
-            
+            print("outputs :", outputs[i])
             # non-linear activation function
             layers.append(model.activation(outputs[-1], model.alphas[i], model.betas[i]))
+            print("layers :", layers[i])
 
         # linear output layer
         layers.append(tf.add(tf.matmul(layers[-1], model.W[-1]), model.b[-1]))
 
-        print("Layers :", layers)
+        #print("Layers :", layers)
+        #diff here
             
         # rescale the output and return
         return tf.add(tf.multiply(layers[-1], model.magnitudes_scale), model.magnitudes_shift)
