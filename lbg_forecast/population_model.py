83a78f2f36c9779a8e2e632d46ba9d1d849d7bf1
fpetri115 <francescopetri115@gmail.com>
2024-12-18 10:39:53 +0000

    back to not using sfr emulator

--- lbg_forecast/population_model.py
+++ lbg_forecast/population_model.py
@@ -71,7 +71,8 @@ def generate_sps_parameters(nsamples, mass_function_prior, dust_prior, csfrd_pri
         log_sfr_ratios = modified_prospector_beta_sfh_prior(csfrd_prior, redshift, mass, 0.3, alpha=False)
     
     #dust params
-    recent_sfrs = sfr_emulator.predict(np.hstack((np.reshape(redshift, (nsamples, 1)), np.reshape(mass, (nsamples, 1)), log_sfr_ratios))) #np.log10(sfh.calculate_recent_sfr(redshift, 10**mass, log_sfr_ratios))
+    #recent_sfrs = sfr_emulator.predict(np.hstack((np.reshape(redshift, (nsamples, 1)), np.reshape(mass, (nsamples, 1)), log_sfr_ratios)))
+    recent_sfrs = np.log10(sfh.calculate_recent_sfr(redshift, 10**mass, log_sfr_ratios))
     dust2, dust_index, dust1 = dust_prior.sample_dust_model(recent_sfrs)
 
     sps_parameters.append(redshift)
