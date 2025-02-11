a0a46395a0ef066ea7536334a94d944b4b7756c9
fpetri115 <francescopetri115@gmail.com>
2025-02-11 21:13:45 +0000

    nden scripts

--- lbg_forecast/nz.py
+++ lbg_forecast/nz.py
@@ -2,6 +2,8 @@
 import numpy as np
 import lbg_forecast.colour_cuts as cuts
 import lbg_forecast.noise as noise
+import lbg_forecast.priors_gp_massfunc as gpmf
+import lbg_forecast.utils as utils
 
 def default_nz_bins():
     
@@ -84,4 +86,65 @@ def calculate_nzs_from_photometry(sps_params, source_photometry, extra=False):
         params = build_sps_samples_object(u_data, g_data, r_data)
         return nzs, colours, params
 
+def calculate_number_densities(real, spsp, selected_spsp, sparams):
 
+    tot_m = np.log10(spsp[real, :, -1])
+    tot_z = spsp[real, :][:, 0]
+    sparams_real = sparams[real, :, :]
+
+    mass_function_prior = gpmf.MassFunctionPrior('.', mean=False)
+
+    dz=0.05
+    dlogm=0.05
+
+    start_z = 0
+    end_z = 7
+    z_bin = np.linspace(start_z, end_z, int((end_z-start_z)/dz))
+    z_midpoint = (z_bin[1:]+z_bin[:-1])/2
+
+    start_logm = 7
+    end_logm = 13
+    m_bin = np.linspace(start_logm, end_logm, int((end_logm-start_logm)/dlogm))
+    m_midpoint = (m_bin[1:]+m_bin[:-1])/2
+
+    u_sel = np.squeeze(selected_spsp[0])
+    u_sel_m = np.log10(u_sel[:, -1])
+    u_sel_z = u_sel[:, 0]
+
+    g_sel = np.squeeze(selected_spsp[1])
+    g_sel_m = np.log10(g_sel[:, -1])
+    g_sel_z = g_sel[:, 0]
+
+    r_sel = np.squeeze(selected_spsp[2])
+    r_sel_m = np.log10(r_sel[:, -1])
+    r_sel_z = r_sel[:, 0]
+
+    u_counts_2d = np.histogram2d(u_sel_z, u_sel_m, bins=(z_bin, m_bin), density=False)[0]
+    g_counts_2d = np.histogram2d(g_sel_z, g_sel_m, bins=(z_bin, m_bin), density=False)[0]
+    r_counts_2d = np.histogram2d(r_sel_z, r_sel_m, bins=(z_bin, m_bin), density=False)[0]
+    tot_counts_2d = np.histogram2d(tot_z, tot_m, bins=(z_bin, m_bin), density=False)[0]
+
+    phi_z_m_2d = np.zeros_like(u_counts_2d)
+    da_2d = np.zeros_like(phi_z_m_2d)
+
+    i = 0
+    for z in z_midpoint:
+        da = dlogm*mass_function_prior.volume_element(z, dz)
+        phi_z_m = mass_function_prior.mass_function(z, m_midpoint, sparams_real)
+        da_2d[i, :] = da
+        phi_z_m_2d[i, :] = phi_z_m
+        i+=1
+
+    p_detection_2d_u = np.divide(u_counts_2d, tot_counts_2d, out=np.zeros_like(u_counts_2d), where=(tot_counts_2d!=0))
+    n_gal_u = np.sum(p_detection_2d_u*phi_z_m_2d*da_2d)
+    n_den_u = n_gal_u/utils.FULL_SKY_DEG2
+
+    p_detection_2d_g = np.divide(g_counts_2d, tot_counts_2d, out=np.zeros_like(g_counts_2d), where=(tot_counts_2d!=0))
+    n_gal_g = np.sum(p_detection_2d_g*phi_z_m_2d*da_2d)
+    n_den_g = n_gal_g/utils.FULL_SKY_DEG2
+
+    p_detection_2d_r = np.divide(r_counts_2d, tot_counts_2d, out=np.zeros_like(r_counts_2d), where=(tot_counts_2d!=0))
+    n_gal_r = np.sum(p_detection_2d_r*phi_z_m_2d*da_2d)
+    n_den_r = n_gal_r/utils.FULL_SKY_DEG2
+
+    return np.array([n_den_u, n_den_g, n_den_r])
--- photo_to_nz.py
+++ photo_to_nz.py
@@ -1,14 +1,9 @@
 import sys
 import lbg_forecast.nz as nz
 import numpy as np
-import lbg_forecast.utils as utils
-import lbg_forecast.priors_gp_massfunc as gpmf
 
 path = sys.argv[1]
 run = sys.argv[2]
-extra = 1# int(sys.argv[3])
-
-mass_function_prior = gpmf.MassFunctionPrior('.', mean=False)
 
 sps_parameters = np.load(path+"sps_parameter_samples/sps_"+run+".npy")
 photometry = np.load(path+"photo_samples/photo_"+run+".npy")
@@ -21,14 +16,14 @@ ntot = photometry.shape[1]
 cs = []
 params = []
 for n in range(photometry.shape[0]):
-    lbg_nzs, lbg_colours, lbg_params = nz.calculate_nzs_from_photometry(sps_parameters[n, :, :], photometry[n, :, :], extra=extra)
-    for i in range(3):
-        pass
-        #n_detected[n, i] = mass_function_prior.normalised_mass_function()/utils.FULL_SKY_DEG2
+    lbg_nzs, lbg_colours, lbg_params = nz.calculate_nzs_from_photometry(sps_parameters[n, :, :], photometry[n, :, :], extra=1)
     nzs.append(lbg_nzs)
     cs.append(lbg_colours)
     params.append(lbg_params)
+
+    n_detected[n, :] = nz.calculate_number_densities(n, sps_parameters, lbg_params, sparams)
     print("Realisation: ", n+1, flush=True)
+
 c_data = np.array(cs)
 params_data = np.array(params)
 np.save(path+"nz_samples/c_"+run+".npy", c_data)
@@ -38,6 +33,5 @@ np.save(path+"sps_parameter_samples/selected_sps_"+run+".npy", params_data)
 nz_data = np.asarray(nzs)
 
 np.save(path+"nz_samples/nz_"+run+".npy", nz_data)
-#np.save(path+"nz_samples/trans_cut_"+run+".npy", trans_cut)
-#np.save(path+"nz_samples/trans_"+run+".npy", trans)
+np.save(path+"nz_samples/n_detected_"+run+".npy", n_detected)
 print("Complete.", flush=True)
